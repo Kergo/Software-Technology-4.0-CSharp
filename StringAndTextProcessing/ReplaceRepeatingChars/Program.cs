8ae729996a68df40bdb0015ac517da6934174396
Kergo <sskamenov@gmail.com>
2019-01-19 00:18:06 +0100

    Replace Repeating Chars

---
+++ StringAndTextProcessing/ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReplaceRepeatingChars
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var output = new StringBuilder();
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] != input[i + 1])
+                {
+                    output.Append(input[i]);
+                }
+            }
+
+            output.Append(input.Last());
+            Console.WriteLine(output);
+        }
+    }
+}
