a82a15d8ebbe7ec6e43c8ec7af7e6d943b3c143d
Kergo <sskamenov@gmail.com>
2019-01-18 21:00:29 +0100

    Encrypting Text

---
+++ StringAndTextProcessing/CaesarCipher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace CaesarCipher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var chars = input.ToCharArray();
+            var encryptedText = new StringBuilder();   
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                int charAsNum = chars[i];
+                charAsNum += 3;
+                var newChar = char.ConvertFromUtf32(charAsNum);
+                encryptedText.Append(newChar);
+            }
+
+            System.Console.WriteLine(encryptedText);
+        }
+    }
+}
