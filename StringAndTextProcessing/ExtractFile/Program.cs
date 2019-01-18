cb3cbe45508b1c4b6f78b87a78484112b16bd070
Kergo <sskamenov@gmail.com>
2019-01-18 20:08:19 +0100

    Extract File

---
+++ StringAndTextProcessing/ExtractFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ExtractFile
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            string[] parts = path.Split('\\');
+            string file = parts[parts.Length - 1]; 
+            string fileName = file.Split('.').First();
+            string extension = file.Split('.').Last();
+
+            System.Console.WriteLine($"File name: {fileName}");
+            System.Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
